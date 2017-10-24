9806a735eac5e287edfaaa52f40577a8758c98a4
lixiaoxiong <lixiaoxiong@benmu-health.com>
2017-10-24 21:38:44 +0800

    sdfa

--- service/src/main/java/com/wangda/alarm/service/tcplayer/RespHeaderDecoder.java
+++ service/src/main/java/com/wangda/alarm/service/tcplayer/RespHeaderDecoder.java
@@ -1,8 +1,10 @@
 package com.wangda.alarm.service.tcplayer;
 
 import com.wangda.alarm.service.bean.AlarmHeader;
+import com.wangda.alarm.service.bean.DataType;
 import com.wangda.alarm.service.bean.RespHeader;
 import com.wangda.alarm.service.bean.protocol.ProtocalFieldsDesc;
+import com.wangda.alarm.service.util.ByteBufferUtil;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.CharsetDecoder;
 import org.apache.mina.core.buffer.IoBuffer;
@@ -18,22 +20,41 @@ public class RespHeaderDecoder {
         //1. 目的电报码
         buffer.position(ProtocalFieldsDesc.RESP_HEADER_TARGET_TELECODE.getPosition());
         buffer.limit(ProtocalFieldsDesc.RESP_HEADER_TARGET_TELECODE.getLimit());
+        String target = buffer.getString(cd);
+        header.setTargetTeleCode(target);
 
         //2. 源电报码
         buffer.position(ProtocalFieldsDesc.RESP_HEADER_SOURCE_TELECODE.getPosition());
         buffer.limit(ProtocalFieldsDesc.RESP_HEADER_SOURCE_TELECODE.getLimit());
+        String source = buffer.getString(cd);
+        header.setSourceTeleCode(source);
 
         //3. 数据命令代码
-        buffer.position(ProtocalFieldsDesc);
-        buffer.limit(ProtocalFieldsDesc);
-        buffer.position(ProtocalFieldsDesc);
-        buffer.limit(ProtocalFieldsDesc);
-        buffer.position(ProtocalFieldsDesc);
-        buffer.limit(ProtocalFieldsDesc);
-        buffer.position(ProtocalFieldsDesc);
-        buffer.limit(ProtocalFieldsDesc);
-        buffer.position(ProtocalFieldsDesc);
-        buffer.limit(ProtocalFieldsDesc);
+        buffer.position(ProtocalFieldsDesc.RESP_HEADER_DATACMD_CODE.getPosition());
+        buffer.limit(ProtocalFieldsDesc.RESP_HEADER_DATACMD_CODE.getLimit());
+        int datacmd = ByteBufferUtil.byteToInt(buffer.get());
+        if (datacmd == 0x00) {
+            header.setDataType(DataType.DATA);
+        } else if (datacmd == 0x01) {
+            header.setDataType(DataType.CMD);
+        }
+
+        //4. 压缩标识
+        buffer.position(ProtocalFieldsDesc.RESP_HEADER_ZIP_FLAG.getPosition());
+        buffer.limit(ProtocalFieldsDesc.RESP_HEADER_ZIP_FLAG.getLimit());
+        header.setZip();
+        //5. 版本码
+        buffer.position(ProtocalFieldsDesc.RESP_HEADER_VERSION.getPosition());
+        buffer.limit(ProtocalFieldsDesc.RESP_HEADER_VERSION.getLimit());
+
+        //6. 数据类型码
+        buffer.position(ProtocalFieldsDesc.RESP_HEADER_DATA_TYPE.getPosition());
+        buffer.limit(ProtocalFieldsDesc.RESP_HEADER_DATA_TYPE.getLimit());
+
+        //7. 数据子类型码
+        buffer.position(ProtocalFieldsDesc.RESP_HEADER_DATA_SUBTYPE.getPosition());
+        buffer.limit(ProtocalFieldsDesc.RESP_HEADER_DATA_SUBTYPE.getLimit());
+
         buffer.position(ProtocalFieldsDesc);
         buffer.limit(ProtocalFieldsDesc);
 
