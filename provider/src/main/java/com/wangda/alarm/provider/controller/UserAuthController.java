a61327c2a966f8021dde2e88f9be0e536fc2f20d
lixiaoxiong <lixiaoxiong@benmu-health.com>
2017-10-27 00:51:16 +0800

    fsadfsd

--- provider/src/main/java/com/wangda/alarm/provider/controller/UserAuthController.java
+++ provider/src/main/java/com/wangda/alarm/provider/controller/UserAuthController.java
@@ -1,10 +1,12 @@
 package com.wangda.alarm.provider.controller;
 
 import com.wangda.alarm.provider.biz.UserAuthBiz;
+import com.wangda.alarm.service.bean.vo.req.UpdatePassReq;
 import com.wangda.alarm.service.common.springconfig.annotation.JsonBody;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -22,5 +24,23 @@ public class UserAuthController {
     @JsonBody
     public void login(String userName, String password, HttpServletResponse response) {
         userAuthBiz.auth(userName, password, response);
+    }
+
+    @RequestMapping("/retrievePass")
+    @JsonBody
+    public void retrievePassword(String userName) {
+
+    }
+
+    @RequestMapping("/updatePassword")
+    @JsonBody
+    public void updatePassword(@RequestBody UpdatePassReq req) {
+
+    }
+
+    @RequestMapping("/logout")
+    @JsonBody
+    public void logout() {
+
     }
 }
